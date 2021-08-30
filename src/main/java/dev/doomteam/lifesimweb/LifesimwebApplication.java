10e659c0b7da934f4b3a05ef3a3cc2f2816d1423
Jose <josersanchez0117@gmail.com>
2021-08-30 12:51:56 -0500

    initial commit

---
+++ src/main/java/dev/doomteam/lifesimweb/LifesimwebApplication.java
@@ -0,0 +1,13 @@
+package dev.doomteam.lifesimweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifesimwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifesimwebApplication.class, args);
+	}
+
+}
