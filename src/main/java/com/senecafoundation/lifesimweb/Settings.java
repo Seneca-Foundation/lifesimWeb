c6959c2a876948c7bb73d88f3fd197525f04ed65
wtvamp <warrenjthompson@gmail.com>
2021-10-17 18:46:53 -0700

    More updates to undo breaking changes from before

--- src/main/java/com/senecafoundation/lifesimweb/Settings.java
+++
@@ -1,7 +0,0 @@
-package com.senecafoundation.lifesimweb;
-
-public class Settings {
-	public static final String SCENE_FILE = "scenes.json";
-	public static final String SAVE_FILE = "savefile.json";
-	public static final String LEADERBOARD_URL = "";
-}
